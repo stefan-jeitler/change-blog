c3ee08735de550e0bcfa781055fd942cb46371cf
stefan <stefan.jeitler>
2021-04-12 21:41:40 +0200

    add extensions tests

---
+++ test/ChangeTracker.Application.Tests/ExtensionsTests/EnumerableExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChangeTracker.Application.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace ChangeTracker.Application.Tests.ExtensionsTests
+{
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void DistinctBy_WithNullSource_EagerArgumentNullException()
+        {
+            Func<IEnumerable<int>> act = () => ((IEnumerable<int>)null).DistinctBy(x => x);
+
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void DistinctBy_WithNullSelector_EagerArgumentNullException()
+        {
+            var sequence = Enumerable.Range(0, 5);
+
+            Func<IEnumerable<int>> act = () => sequence.DistinctBy((Func<int, bool>)null);
+
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+    }
+}

3ddd5accfb8f0d34795e2b83e118936639e25168
Stefan <stefan.jeitler>
2021-07-31 13:14:46 +0200

    fix naming issues in AccountProductRoleDto

--- src/ChangeTracker.Application/DataAccess/Users/AccountProductRolesDto.cs
+++ src/ChangeTracker.Application/DataAccess/Users/AccountProductRolesDto.cs
@@ -10,17 +10,17 @@ namespace ChangeTracker.Application.DataAccess.Users
         public AccountProductRolesDto(IEnumerable<Role> accountRoles,
             IEnumerable<Role> productRoles)
         {
-            AccountPermissions = accountRoles ?? throw new ArgumentNullException(nameof(accountRoles));
-            ProductPermissions = productRoles ?? throw new ArgumentNullException(nameof(productRoles));
+            AccountRoles = accountRoles ?? throw new ArgumentNullException(nameof(accountRoles));
+            ProductRoles = productRoles ?? throw new ArgumentNullException(nameof(productRoles));
         }
 
-        public IEnumerable<Role> AccountPermissions { get; }
-        public IEnumerable<Role> ProductPermissions { get; }
+        public IEnumerable<Role> AccountRoles { get; }
+        public IEnumerable<Role> ProductRoles { get; }
 
-        public void Deconstruct(out IEnumerable<Role> accountRole, 
-            out IEnumerable<Role> productRole)
+        public void Deconstruct(out IEnumerable<Role> accountRoles, 
+            out IEnumerable<Role> productRoles)
         {
-            (accountRole, productRole) = (AccountPermissions, ProductPermissions);
+            (accountRoles, productRoles) = (AccountRoles, ProductRoles);
         }
     }
 }

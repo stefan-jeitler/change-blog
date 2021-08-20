6807a3e43caf996dc58c137533eccfa03314c762
Stefan Jeitler <stefan.jeitler@outlook.com>
2021-08-20 06:04:20 +0000

    Update comment in SkipAuthorizationAttribute.cs

--- src/ChangeBlog.Api/Authorization/SkipAuthorizationAttribute.cs
+++ src/ChangeBlog.Api/Authorization/SkipAuthorizationAttribute.cs
@@ -6,7 +6,7 @@ namespace ChangeBlog.Api.Authorization
     /// Use it with care in situations where no resource id is provided in the request.
     /// For instance, the endpoint to get all products for a user does not contain a resource id in the request.
     /// Therefore, the AuthorizationFilter cannot authorize the request.
-    /// You must find all products in all accounts to which the user belongs.
+    /// You must find all products in all accounts to which the user belongs on your own.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
     public class SkipAuthorizationAttribute : Attribute

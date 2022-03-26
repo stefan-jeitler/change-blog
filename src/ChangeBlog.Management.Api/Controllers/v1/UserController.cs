b52ce421a84525100f9bb8cb90aafea9044c7780
stefan.jeitler <stefan.jeitler@outlook.com>
2022-03-26 12:37:20 +0100

    remove user info endpoint from management api

--- src/ChangeBlog.Management.Api/Controllers/v1/UserController.cs
+++ src/ChangeBlog.Management.Api/Controllers/v1/UserController.cs
@@ -1,10 +1,6 @@
 ﻿using System.Net.Mime;
-using System.Threading.Tasks;
 using ChangeBlog.Api.Shared.DTOs;
 using ChangeBlog.Api.Shared.Swagger;
-using ChangeBlog.Application.UseCases.Queries.GetUsers;
-using ChangeBlog.Management.Api.DTOs;
-using ChangeBlog.Management.Api.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,13 +14,4 @@ namespace ChangeBlog.Management.Api.Controllers.v1;
 [SwaggerControllerOrder(1)]
 public class UserController : ControllerBase
 {
-    [HttpGet("info")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<ActionResult<UserDto>> GetUserInfoAsync([FromServices] IGetUsers getUser)
-    {
-        var userId = HttpContext.GetUserId();
-        var user = await getUser.ExecuteAsync(userId);
-
-        return Ok(UserDto.FromResponseModel(user));
-    }
 }

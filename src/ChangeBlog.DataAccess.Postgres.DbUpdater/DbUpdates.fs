96ff3fa2c113bb8241c611393d92cc953565e50f
stefan.jeitler <stefan.jeitler@outlook.com>
2022-04-19 18:45:25 +0200

    add user data table

--- src/ChangeBlog.DataAccess.Postgres.DbUpdater/DbUpdates.fs
+++ src/ChangeBlog.DataAccess.Postgres.DbUpdater/DbUpdates.fs
@@ -57,4 +57,5 @@ let dbUpdates: DbUpdate list =
       { Version = v "12.1.3"; Update = VersioningSchemeTable.fixUniqueIndexOnNameAndAccountId }
       { Version = v "12.1.4"; Update = VersionTable.fixUniqueIndexOnProductIdAndValue }
       { Version = v "12.1.5"; Update = VersionTable.addProductIdToSearchVectorIndex }
-      { Version = v "13.0.0"; Update = UserTable.addCultureColumn } ]
+      { Version = v "13.0.0"; Update = UserTable.addCultureColumn }
+      { Version = v "13.1.0"; Update = UserDataTable.create } ]
---
+++ src/ChangeBlog.DataAccess.Postgres.DbUpdater/DbUpdates/UserDataTable.fs
@@ -0,0 +1,17 @@
+﻿module UserDataTable
+
+open System.Data
+open Dapper
+
+let private createSql = """
+        CREATE TABLE IF NOT EXISTS user_data (
+        user_id UUID PRIMARY KEY,
+        data JSONB CONSTRAINT userdata_data_nn NOT NULL,
+        updated_at TIMESTAMP CONSTRAINT userdata_updatedat_nn NOT NULL,
+        CONSTRAINT userdata_userid_fkey FOREIGN KEY (user_id) REFERENCES "user"(id)
+        )
+    """
+    
+let create (dbConnection: IDbConnection) =
+    dbConnection.Execute(createSql)
+    |> ignore

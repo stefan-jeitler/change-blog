6dc582c24d2756831ab8f262cbef767ffc6e505e
Stefan <stefan.jeitler@outlook.com>
2022-04-30 20:36:12 +0200

    set cache header properly

--- src/ChangeBlog.Management.Api/Startup.cs
+++ src/ChangeBlog.Management.Api/Startup.cs
@@ -66,15 +66,13 @@ public class Startup
 
         app.UseStaticFiles();
 
-        if (!env.IsDevelopment())
-            app.UseSpaStaticFiles(new StaticFileOptions
+        app.UseSpaStaticFiles(new StaticFileOptions
+        {
+            OnPrepareResponse = context =>
             {
-                OnPrepareResponse = context =>
-                {
-                    context.Context.Response.Headers.Add("Cache-Control", "max-age=31536000");
-                    context.Context.Response.Headers.Add("Expires", "31536000");
-                }
-            });
+                context.Context.Response.Headers.Add("Cache-Control", "no-cache");
+            }
+        });
 
         app.UseRouting();
 
@@ -91,20 +89,7 @@ public class Startup
         app.UseSpa(spa =>
         {
             spa.Options.SourcePath = "ClientApp";
-            
-            spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
-            {
-                OnPrepareResponse = context =>
-                {
-                    // never cache index.html
-                    if (context.File.Name == "index.html")
-                    {
-                        context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
-                        context.Context.Response.Headers.Add("Expires", "-1");
-                    }
-                }
-            };
-            
+
             if (env.IsDevelopment())
                 spa.UseAngularCliServer("start");
         });

e06e0c02d95c5c88067fe4237fed2f26d0c97562
Stefan <stefan.jeitler@outlook.com>
2022-05-01 20:07:55 +0200

    update swipe config

--- src/ChangeBlog.Management.Api/ClientApp/src/app/configuration/swipe-config.service.ts
+++ src/ChangeBlog.Management.Api/ClientApp/src/app/configuration/swipe-config.service.ts
@@ -5,8 +5,11 @@ import * as Hammer from "hammerjs";
 @Injectable()
 export class SwipeConfig extends HammerGestureConfig {
   overrides = <any>{
-    swipe: {direction: Hammer.DIRECTION_HORIZONTAL},
+    swipe: {direction: Hammer.DIRECTION_RIGHT},
     pinch: { enable: false },
-    rotate: { enable: false }
-  };
+    rotate: { enable: false },
+    pan: {enable: false},
+    press: {enable: false},
+    tap: {enable: false}
+  }
 }

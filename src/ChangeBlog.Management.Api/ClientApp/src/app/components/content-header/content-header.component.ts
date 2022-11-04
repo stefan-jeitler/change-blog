96a5bc28b10516fa32bd9688786aecf0ee220344
Stefan <stefan.jeitler@outlook.com>
2022-11-05 00:21:36 +0100

    add back button option to header component

--- src/ChangeBlog.Management.Api/ClientApp/src/app/components/content-header/content-header.component.ts
+++ src/ChangeBlog.Management.Api/ClientApp/src/app/components/content-header/content-header.component.ts
@@ -1,19 +1,32 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Location} from "@angular/common";
 
 @Component({
-  selector: 'app-content-header',
-  templateUrl: './content-header.component.html',
-  styleUrls: ['./content-header.component.scss']
+    selector: 'app-content-header',
+    templateUrl: './content-header.component.html',
+    styleUrls: ['./content-header.component.scss']
 })
 export class ContentHeaderComponent implements OnInit {
 
-  @Input() title: string;
+    @Input() title: string;
+    @Input() showBackButton: boolean;
+    @Input() goBackUrl: string;
 
-  constructor() {
-    this.title = '';
-  }
+    constructor(private router: Router,
+                private location: Location) {
+        this.title = '';
+        this.showBackButton = false;
+        this.goBackUrl = '';
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    async goBack() {
+        if (!!this.goBackUrl)
+            await this.router.navigateByUrl(this.goBackUrl);
+        else
+            this.location.back();
+    }
 }

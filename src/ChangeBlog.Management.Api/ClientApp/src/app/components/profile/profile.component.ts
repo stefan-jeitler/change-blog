ca9a5987c276546be4b86cabfb5cd2bae144113c
Stefan <stefan.jeitler@outlook.com>
2022-11-08 18:42:48 +0100

    show either content or loading spinner, but not overlay loading spinner

--- src/ChangeBlog.Management.Api/ClientApp/src/app/components/profile/profile.component.ts
+++ src/ChangeBlog.Management.Api/ClientApp/src/app/components/profile/profile.component.ts
@@ -43,7 +43,6 @@ export class ProfileComponent implements OnInit {
     }
 
     async ngOnInit(): Promise<void> {
-
         try {
             await this.loadAvailableDropdownOptions();
             this.loadUserProfile()

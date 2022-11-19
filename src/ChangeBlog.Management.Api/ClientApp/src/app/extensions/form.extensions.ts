a77735cb09510d57f8d99ec21a23af1a0d1ef54f
Stefan <stefan.jeitler@outlook.com>
2022-11-19 23:52:27 +0100

    format form.extensions

--- src/ChangeBlog.Management.Api/ClientApp/src/app/extensions/form.extensions.ts
+++ src/ChangeBlog.Management.Api/ClientApp/src/app/extensions/form.extensions.ts
@@ -5,39 +5,39 @@ import ErrorMessage = ChangeBlogManagementApi.ErrorMessages;
 
 declare module "@angular/forms" {
 
-    interface FormGroup {
-        resetValidation(this: FormGroup): void;
+  interface FormGroup {
+    resetValidation(this: FormGroup): void;
 
-        setErrors(this: FormGroup, errorMessages: ErrorMessage[]): void;
+    setErrors(this: FormGroup, errorMessages: ErrorMessage[]): void;
 
-        showErrorMessage(this: FormGroup, formControlName: string): boolean;
+    showErrorMessage(this: FormGroup, formControlName: string): boolean;
 
-        getErrorMessages(this: FormGroup, formControlName: string): string;
-    }
+    getErrorMessages(this: FormGroup, formControlName: string): string;
+  }
 }
 
 UntypedFormGroup.prototype.resetValidation = function (this: UntypedFormGroup) {
-    for (const key of Object.keys(this.controls)) {
-        const control = this.controls[key];
-        control.setErrors(null);
-        control.markAsUntouched();
-    }
+  for (const key of Object.keys(this.controls)) {
+    const control = this.controls[key];
+    control.setErrors(null);
+    control.markAsUntouched();
+  }
 }
 
 UntypedFormGroup.prototype.setErrors = function (this: UntypedFormGroup, errorMessages: ErrorMessage[]) {
-    for (const e of errorMessages.filter((x: ErrorMessage) => !!x.property)) {
-        const formControl = this.get(e.property!);
-        const fieldValidationErrors = new ValidationError(e?.messages ?? []);
+  for (const e of errorMessages.filter((x: ErrorMessage) => !!x.property)) {
+    const formControl = this.get(e.property!);
+    const fieldValidationErrors = new ValidationError(e?.messages ?? []);
 
-        formControl?.setErrors(fieldValidationErrors);
-    }
+    formControl?.setErrors(fieldValidationErrors);
+  }
 }
 
 UntypedFormGroup.prototype.showErrorMessage = function (this: UntypedFormGroup, formControlName: string) {
-    const control = this.get(formControlName);
-    return (control?.invalid ?? false) && control?.errors instanceof ValidationError;
+  const control = this.get(formControlName);
+  return (control?.invalid ?? false) && control?.errors instanceof ValidationError;
 }
 
 UntypedFormGroup.prototype.getErrorMessages = function (this: UntypedFormGroup, formControlName: string) {
-    return this.get(formControlName)?.errors?.messages ?? [];
+  return this.get(formControlName)?.errors?.messages ?? [];
 }
